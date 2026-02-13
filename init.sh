03023347f4927b03971ebb5b1835b1195784cb09
Lotoss25 <lotoss25@gmail.com>
2026-02-13 07:48:07 +0000

    update init.sh

--- init.sh
+++ init.sh
@@ -5,29 +5,53 @@ RUNNER_TOKEN=$1
 # 2. Запитуємо у API номер останньої версії (вирізаємо саме цифри, наприклад 2.311.0)
 LATEST_VERSION=$(curl -s https://api.github.com/repos/actions/runner/releases/latest | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
 
+
 if [ -z "$RUNNER_TOKEN" ]; then
     echo "❌ Помилка: Не вказано токен!"
     echo "Використання: ./init.sh ВАШ_ТОКЕН"
     exit 1
 fi
 
-# 3. Ставимо софт
-echo "📦 Встановлюємо Git та Ansible..."
-sudo apt update
-sudo apt install -y git software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install -y ansible
+# Перевіряємо яка система встановлена
+. /etc/os-release
+GH_ARCH=$(dpkg --print-architecture) 
+if [[ $ID == 'debian' && $GH_ARCH == 'arm64' ]]; then
+  echo "📦 Встановлюємо Git та Ansible..."
+  sudo apt update
+  sudo apt install -y git software-properties-common
+  sudo apt install -y git python3-pip
+  sudo apt remove -y ansible
+  sudo pip3 install ansible
+  # 4. Готуємо ранера
+  echo "🏃 Налаштовуємо GitHub Runner..."
+  mkdir -p actions-runner && cd actions-runner
 
-# 4. Готуємо ранера
-echo "🏃 Налаштовуємо GitHub Runner..."
-mkdir -p actions-runner && cd actions-runner
+  #  Скачуємо (це посилання краще брати свіже з сайту GitHub, але для прикладу хай буде)
+  curl -o actions-runner-linux-arm64-${LATEST_VERSION}.tar.gz -L https://github.com/actions/runner/releases/download/v${LATEST_VERSION}/actions-runner-linux-arm64-${LATEST_VERSION}.tar.gz
 
-# Скачуємо (це посилання краще брати свіже з сайту GitHub, але для прикладу хай буде)
-curl -o actions-runner-linux-x64-${LATEST_VERSION}.tar.gz -L https://github.com/actions/runner/releases/download/v${LATEST_VERSION}/actions-runner-linux-x64-${LATEST_VERSION}.tar.gz
+  # Розпаковуємо
+  tar xzf ./actions-runner-linux-arm64-${LATEST_VERSION}.tar.gz
 
-# Розпаковуємо
-tar xzf ./actions-runner-linux-x64-${LATEST_VERSION}.tar.gz
 
+else 
+  # 3. Ставимо софт
+  echo "📦 Встановлюємо Git та Ansible..."
+  sudo apt update
+  sudo apt install -y git software-properties-common
+  sudo apt-add-repository --yes --update ppa:ansible/ansible
+  sudo apt install -y ansible
+  # 4. Готуємо ранера
+  echo "🏃 Налаштовуємо GitHub Runner..."
+  mkdir -p actions-runner && cd actions-runner
+
+  # Скачуємо (це посилання краще брати свіже з сайту GitHub, але для прикладу хай буде)
+  curl -o actions-runner-linux-x64-${LATEST_VERSION}.tar.gz -L https://github.com/actions/runner/releases/download/v${LATEST_VERSION}/actions-runner-linux-x64-${LATEST_VERSION}.tar.gz
+
+  # Розпаковуємо
+  tar xzf ./actions-runner-linux-x64-${LATEST_VERSION}.tar.gz
+
+
+fi
 # Налаштовуємо (з вашим токеном!)
 ./config.sh --url https://github.com/Lotoss25/my_k8s --token $RUNNER_TOKEN --unattended
 
